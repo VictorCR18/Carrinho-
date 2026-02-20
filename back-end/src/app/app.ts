64fa14bd820841fc517df2541af62de5e5c83899
Victor Cavalcante <cavalcantevictor2003@gmail.com>
2026-02-20 11:11:43 -0300

    feat: aumentando o limite de upload de pacote para 50mb

--- back-end/src/app/app.ts
+++ back-end/src/app/app.ts
@@ -14,10 +14,13 @@ app.use(
     credentials: true,
   }),
 );
-app.use(express.json());
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(router);
 
-app.get("/hello", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Backend funcionando!" });
 });
 
